6728e2ad8b63d84898bd44c605144244d27df0a6
Jerome Rojas <skreamlight@gmail.com>
2025-05-16 14:11:28 -0400

    urls hotfix

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     
     path('crud/', views.crud, name='crud'),
-    path('register/', views.register),
+    path('register_2/', views.register),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('update/', views.update, name='update'),
     path('delete/<int:id>', views.delete, name='delete'),
