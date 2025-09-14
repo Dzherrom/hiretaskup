0a1c93ae693bdaf6ff9bf37c7f454fed78f97cf6
jerome <skreamlight@gmail.com>
2025-09-14 15:19:11 -0600

    signin/signup site styles

--- core/forms.py
+++ core/forms.py
@@ -6,6 +6,28 @@ from .models import Meeting
 User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username',
+            'class': 'input-field',
+            'autocomplete': 'username',
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Email',
+            'class': 'input-field',
+            'autocomplete': 'email',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Password',
+            'class': 'input-field',
+            'autocomplete': 'new-password',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm Password',
+            'class': 'input-field',
+            'autocomplete': 'new-password',
+        })
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
