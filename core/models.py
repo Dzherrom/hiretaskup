dc3761a8f3333750ed611dee351d8f553747b8d6
jerome <skreamlight@gmail.com>
2025-10-29 17:42:48 -0600

    local fix

--- core/models.py
+++ core/models.py
@@ -26,7 +26,7 @@ class VirtualAssistant(models.Model):
     def __str__(self):
         return self.name
 
-
+#damn
 class Subscription(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='subscriptions')
     plan_name = models.CharField(max_length=100)
