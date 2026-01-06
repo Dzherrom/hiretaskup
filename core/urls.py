e5b87322acb0d3b35b677fe38cf4ef5903033442
jerome <skreamlight@gmail.com>
2026-01-06 00:49:28 -0600

    login background

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('users/', views.user_list, name='user_list'),
     path('users/profile/', views.user_profile, name='user_profile'),
     path('users/create/', views.user_create, name='user_create'),
     path('users/edit/<int:id>/', views.user_edit, name='user_edit'),
